d7de684ee3d060dc2230072a7555bd31fa5812f7
Manikant Jha <manikantcodes@gmail.com>
2024-12-02 10:13:36 +0530

    chart integration

--- projects/inventoryManagement/js/index.js
+++ projects/inventoryManagement/js/index.js
@@ -1,2 +1,41 @@
-// const container = document.getElementById("container");
-const body = document.body;
+const myCanvas = document.getElementById("myChart");
+
+const inventory = JSON.parse(localStorage.getItem("inventory")) || [];
+const orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+const labels = inventory.map((value) => {
+  return value.name;
+});
+
+const temp = {};
+
+for (const item of orders) {
+  if (temp[item.name]) {
+    temp[item.name]++;
+  } else {
+    temp[item.name] = 1;
+  }
+}
+
+console.log("temp", temp);
+
+const sellingQty = new Chart(myCanvas, {
+  type: "bar",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "# of Votes",
+        data: Object.values(temp),
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
