d359bfd3e5f314a2bba6593666bf78bf4397973e
Manikant Jha <manikantcodes@gmail.com>
2024-12-06 18:55:54 +0530

    dom

---
+++ theory/dom/index.js
@@ -0,0 +1,18 @@
+// const h1 = document.getElementById("title");
+// h1.style.color = "red";
+// console.log("h1.innerHTML", h1.innerHTML);
+// h1.innerHTML = "Learning JavaScript!";
+
+// const paragraphs = document.getElementsByTagName("p");
+// paragraphs[0].style.color = "red";
+// paragraphs[1].style.color = "red";
+// paragraphs[2].style.color = "red";
+// paragraphs[3].style.color = "red";
+// for (const p of paragraphs) {
+//   p.style.backgroundColor = "red";
+// }
+
+const anchorTag = document.getElementById("mylink");
+anchorTag.href = "/theory/dom/contact.html";
+
+console.log("anchorTag", anchorTag);
