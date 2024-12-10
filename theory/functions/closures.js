4c7d99a47a8a974247719ef7d7422dc2a3931a25
Manikant Jha <manikantcodes@gmail.com>
2024-12-10 10:53:24 +0530

    closures and grid

---
+++ theory/functions/closures.js
@@ -0,0 +1,24 @@
+// function setup() {
+//   let count = 0; // Only Once
+
+//   function increment() {
+//     count++; // Multiple Times | From Outside
+//     console.log("count", count); // Multiple Times | From Outside
+//   }
+
+//   function decrement() {
+//     count--; // Multiple Times | From Outside
+//     console.log("count", count); // Multiple Times | From Outside
+//   }
+
+//   return { increment, decrement };
+// }
+
+// const { increment, decrement } = setup();
+
+// increment();
+// increment();
+// increment();
+
+// decrement();
+// decrement();
