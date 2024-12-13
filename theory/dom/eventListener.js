0da4b23dde6b1a53b5e4f255ef23f1a32b9c634f
Manikant Jha <manikantcodes@gmail.com>
2024-12-13 18:22:10 +0530

    event listeners

---
+++ theory/dom/eventListener.js
@@ -0,0 +1,15 @@
+// const mybtn = document.getElementById("mybtn");
+
+// function changeTitle() {
+//   document.getElementById("mytitle").innerHTML = "New Title";
+// }
+
+// function consoleLogTitle() {
+//   console.log("New Title");
+// }
+
+// mybtn.onclick = changeTitle;
+// mybtn.onclick = consoleLogTitle;
+
+// mybtn.addEventListener("click", changeTitle);
+// mybtn.addEventListener("click", consoleLogTitle);
---
+++ theory/dom/navigation.js
@@ -0,0 +1,13 @@
+// const container = document.getElementById("container");
+
+// console.log(container.children[2]);
+// console.log(container.parentElement);
+// console.log(container.firstElementChild);
+// console.log(container.lastElementChild);
+// console.log(container.nextElementSibling);
+// console.log(container.previousElementSibling);
+
+// console.log(container.childNodes);
+// console.log(container.parentNode);
+// console.log(container.nextSibling);
+// console.log(container.previousSibling);
